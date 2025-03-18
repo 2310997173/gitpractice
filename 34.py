2fe1283882ed63c80fe5b8e3ab262afc3913e6b0
Mithil_OOPS <mithil7173.ca23@chitkara.edu.in>
2025-03-18 10:32:19 +0530

    python codes

--- 34.py
+++ 34.py
@@ -49,3 +49,19 @@ print(myfamily["child2"]["Name"])
 student1={}
 for _ in range(10):
     roll_no = input["Enter the roll no: "]
+    student1[roll_no] = {
+        "Name" : input("Enter the name: "),
+        "Age" : input("Enter the age: "),
+        "Course" : input("Enter your course: "),
+        "Block" : input("Enter your block name: ")
+    }
+    print("Student data is added")
+Search_roll_no = input("Enter the roll no you want to search: ")
+if Search_roll_no in student1:
+    print("Student Details")
+    print("Name: ",student1[Search_roll_no]["Name"])
+    print("Age: ",student1[Search_roll_no]["Age"])
+    print("Course: ",student1[Search_roll_no]["Course"])
+    print("Block: ",student1[Search_roll_no]["Block"])
+else:
+    print("Record not found!")    
