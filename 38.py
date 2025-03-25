bf64462d97a7bdac72743b634cc2bfa05c9c9611
Mithil_OOPS <mithil7173.ca23@chitkara.edu.in>
2025-03-25 10:25:15 +0530

    python codes

--- 38.py
+++ 38.py
@@ -7,5 +7,6 @@ def my_function(a,b,*age,c = 0,d = 0,**keyword):
     total = a+b+c+d
     total = total+sum(age)
     print(total)
+    
 my_function(10,20,30,40,c = 5,d = 10,
             fname = "Ram",lname = "Sharma")
