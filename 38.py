5c132026e344e105b76eec38e8b58ab9e7df4502
Mithil_OOPS <mithil7173.ca23@chitkara.edu.in>
2025-03-25 10:30:46 +0530

    python codes

--- 38.py
+++ 38.py
@@ -1,11 +1,11 @@
-def my_function(a,b,*age,c = 0,d = 0,**keyword):
-    print("Postional assignment: ",a,b)
-    print("Extra Postional argument: ",age)
-    print("Keyword argument: ",c,d)
-    print("Extra kewyword are: ",keyword)
+def my_function(a,b,*arg,c = 0,d = 0,**keyword):
+    print("Postional assignment:",a,b)
+    print("Extra Postional argument:",arg)
+    print("Keyword argument:",c,d)
+    print("Extra kewyword are:",keyword)
 
     total = a+b+c+d
-    total = total+sum(age)
+    total = total+sum(arg)
     print(total)
 
-my_function(10,20,30,40,c = 5,d = 10,fname = "Ram",lname = "Sharma")
+my_function(10,20,30,40,c = 5,d = 10,fname = "Ritika",lname = "Sharma")
