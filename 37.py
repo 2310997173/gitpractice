b0ca49821f9988d543e28b36fa1da12da4ed74aa
Mithil_OOPS <mithil7173.ca23@chitkara.edu.in>
2025-03-25 10:08:47 +0530

    python codes

--- 37.py
+++ 37.py
@@ -3,7 +3,16 @@
 # c = name("Ram")
 # print(c)
 
-def name(*kids):
-    print("The child name is: ",kids[2])
-c = name("Ram","Sita","Sham")
-print(c)    
+# def name(*kids):
+#     print("The child name is: ",kids[2])
+# c = name("Ram","Sita","Sham")
+# print(c)
+
+def name(child1,child2,child3):
+    print("The youngest chi;ld name is: ",child2)
+c = name(child1 = "Ram",
+         child2 = "Sham",
+         child3 = "Sita"
+        )
+
+print(c)
