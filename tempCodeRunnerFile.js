65e1172c94ec8d8a052266463722eda53a1a8ab8
Mithil_OOPS <mithil7173.ca23@chitkara.edu.in>
2025-04-28 13:48:38 +0530

    java codes

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,1 @@
-
-console.log("List after PUSH: ",fruits);
-fruits.pop();
-console.log("Updation after POP: ");
-console.log(fruits);
-console.log("Updation after szice: ");
-console.log(fruits.slice(2.4));
+(() => {
